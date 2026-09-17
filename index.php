86964b5612862c1c6cc8f2e4b85e4a26774f342f
125054306-mtt <125054306@upq.edu.mx>
2026-09-17 17:20:32 -0600

    Trabajamos con estructura de HTML y php

---
+++ index.php
@@ -0,0 +1,36 @@
+<!-- <?php
+    echo "Hola Estrellitas";
+?> -->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        h1{
+            /* TEXTO */
+            color: #451579;
+        }
+        div{
+            /*FONDO*/
+            background: #d447b1;
+        }
+    </style>
+</head>
+<body>
+    <h1>Hola Estrellitas</h1>
+    <h2>Hola Estrellitas</h2>
+    <h3>Hola Estrellitas</h3>
+    <h4>Hola Estrellitas</h4>
+    <h5>Hola Estrellitas</h5>
+    <h6>Hola Estrellitas</h6>
+
+    <div>
+        <h2>Título del párrafo</h2>
+
+        <p>Este es un párrafo</p>
+    </div>
+</body>
+</html>
